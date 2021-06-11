a535075bc8c2a23b1913e60c06cacb936e95639e
Mauri <paparelli3111996@gmail.com>
2021-06-11 11:29:39 +0200

    first commit

---
+++ concessionario/src/test/java/it/ntt/concessionario/ConcessionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ntt.concessionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
